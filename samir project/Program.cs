3f27a279a948fdf84dd1c01ca202c8b62acf35ea
nassermoham88-dotcom <nassermoham88@gmail.com>
2026-05-03 19:20:22 +0300

    Add files via upload

---
+++ samir project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace samir_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
